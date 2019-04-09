af51cab023bfff629410cc7d74812153aa670b88
SubrotoRoy <42340771+SubrotoRoy@users.noreply.github.com>
2019-04-09 16:46:46 +0530

    SonarGo Initial Commit

---
+++ math.go
@@ -0,0 +1,22 @@
+package SonarGoProject
+
+func Add(nums... int) int{
+	var result int
+
+	if len(nums)==0{
+		println("No arguments passed")
+	}
+
+	for _,i:= range nums{
+		result+=i
+	}
+	return result
+}
+
+func Subtract(initial int, nums ... int) int{
+
+	for _, i := range nums{
+		initial -= i
+	}
+	return initial
+}
